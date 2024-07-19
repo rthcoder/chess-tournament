c70f902c029ec8548162e9c0b118cca00ef6f4b4
Tohir <tohirdavrsmart@gmail.com>
2024-07-19 14:45:16 +0500

    finish User model

---
+++ src/models/user.model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    is_admin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+export default mongoose.model("User", userSchema);
